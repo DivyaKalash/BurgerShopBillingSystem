4a584df70e858a1c2e1ffb6162ed0e61b0996c07
DivyaKalash <67993272+DivyaKalash@users.noreply.github.com>
2020-07-22 19:17:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,15 +7,15 @@ public class Main {
         Burger burger = new Burger("Basic","Sausage",123.87,"white");
         burger.addBurgerAddition1("Cheese",30.65);
         burger.addBurgerAddition2("Lettuce", 24.87);
-        System.out.println("Amount to be paid for " +burger.getName()+ " is "+burger.itemizeBurger()+"\n");
+        System.out.println("Amount to be paid for " +burger.getName()+ " is "+String.format("%.2f",burger.itemizeBurger())+"\n");
 
         HealthyBurger healthyBurger = new HealthyBurger("Bacon",198.76);
         healthyBurger.addHealthAddition1Name("egg", 23.98);
         healthyBurger.addHealthAddition2Name("Lentils",17.54);
-        System.out.println("Amount to be paid for " +healthyBurger.getName()+ " is "+healthyBurger.itemizeBurger()+"\n");
+        System.out.println("Amount to be paid for " +healthyBurger.getName()+ " is "+String.format("%.2f",healthyBurger.itemizeBurger())+"\n");
 
         DeluxeBurger deluxeBurger = new DeluxeBurger();
         deluxeBurger.addBurgerAddition3("Should not do this ",87.98);
-        System.out.println("Amount to be paid for " +deluxeBurger.getName()+ " is "+deluxeBurger.itemizeBurger());
+        System.out.println("Amount to be paid for " +deluxeBurger.getName()+ " is "+String.format("%.2f",deluxeBurger.itemizeBurger()));
     }
 }
